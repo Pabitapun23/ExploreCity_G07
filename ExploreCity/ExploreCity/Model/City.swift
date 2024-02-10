e8bc4cfe7d8d2c8f2902916ef7c3e5df1dcc64f0
pc22 <isaaclpc0622@gmail.com>
2024-02-10 00:41:33 -0500

    Added ActivityList, City, Activity, DataManager. Origanized Folder

---
+++ ExploreCity/ExploreCity/Model/City.swift
@@ -0,0 +1,8 @@
+//
+//  City.swift
+//  ExploreCity
+//
+//  Created by Isaac Lee on 10/2/2024.
+//
+
+import Foundation
---
+++ ExploreCity/ExploreCity/View/ActivityList.swift
@@ -0,0 +1,18 @@
+//
+//  ActivityList.swift
+//  ExploreCity
+//
+//  Created by Isaac Lee on 10/2/2024.
+//
+
+import SwiftUI
+
+struct ActivityList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActivityList()
+}
