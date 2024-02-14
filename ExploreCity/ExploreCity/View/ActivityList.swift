c0d532e65621ed713ca63d2088547082e13adb87
pc22 <isaaclpc0622@gmail.com>
2024-02-14 17:04:12 -0500

    Add search feature to the List

--- ExploreCity/ExploreCity/View/ActivityList.swift
+++ ExploreCity/ExploreCity/View/ActivityList.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ActivityList: View {
     var cityList = DataManager.shared.cityList
+    @State private var searchText: String = ""
     
     var body: some View {
         NavigationStack{
             List{
-                ForEach(cityList){ city in
+                ForEach(searchCity(searchTerm: searchText)){ city in
                     Section("Activity in \(city.name)"){
-                        ForEach(city.activityList){ activity in
+                        ForEach(searchActivity(searchTerm: searchText, searchCity: city)){ activity in
                             NavigationLink(destination: ActivityDetailsScreen(activity: activity)){
                                 HStack(alignment: .top){
                                     if let imageURL = activity.photo {
@@ -55,8 +56,43 @@ struct ActivityList: View {
                 }
             }
             .navigationTitle("Activity List")
+            .searchable(text: $searchText, prompt: "Search by activity name")
         }
     }
+    
+    // Check whether the city contains that activity
+    private func searchCity(searchTerm: String) -> [City]{
+        if(searchTerm.isEmpty){
+            return cityList
+        }else{
+            var resultCity: [City] = []
+            for city in self.cityList{
+                for activity in city.activityList {
+                    if( activity.name.contains(searchTerm)){
+                        resultCity.append(city)
+                        break
+                    }
+                }
+            }
+            return resultCity
+        }
+    }
+    
+    // Search activity within the given city
+    private func searchActivity(searchTerm: String, searchCity: City) -> [Activity]{
+        if(searchTerm.isEmpty){
+            return searchCity.activityList
+        }else{
+            var resultActivity: [Activity] = []
+            for activity in searchCity.activityList {
+                if activity.name.contains(searchTerm){
+                    resultActivity.append(activity)
+                }
+            }
+            return resultActivity
+        }
+    }
+        
 }
 
 #Preview {
