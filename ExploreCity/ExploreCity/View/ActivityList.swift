629e1e776efb2b1e728a8183ab606d05be51406c
pc22 <isaaclpc0622@gmail.com>
2024-02-14 17:20:54 -0500

    Added City Filter

--- ExploreCity/ExploreCity/View/ActivityList.swift
+++ ExploreCity/ExploreCity/View/ActivityList.swift
@@ -11,8 +11,20 @@ struct ActivityList: View {
     var cityList = DataManager.shared.cityList
     @State private var searchText: String = ""
     
+    @State private var selectOption = -1
+    let cityOptions:[String] = DataManager.shared.cityList.map { $0.name }
+    
     var body: some View {
         NavigationStack{
+            
+            // Filter City
+            Picker("Select a city", selection: $selectOption) {
+                Text("All City").tag(-1)
+                ForEach(0..<self.cityOptions.count){
+                    Text("\(cityOptions[$0])")
+                }
+            }
+            
             List{
                 ForEach(searchCity(searchTerm: searchText)){ city in
                     Section("Activity in \(city.name)"){
@@ -62,6 +74,18 @@ struct ActivityList: View {
     
     // Check whether the city contains that activity
     private func searchCity(searchTerm: String) -> [City]{
+        if selectOption != -1 {
+            // A City is select
+            var resultCity: [City] = []
+            for city in self.cityList{
+                if city.name.contains(cityOptions[selectOption]){
+                    resultCity.append(city)
+                }
+            }
+            return resultCity
+        }
+        
+        
         if(searchTerm.isEmpty){
             return cityList
         }else{
